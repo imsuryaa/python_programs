759af6c91d1648de208e0ae4aae0c656d727c35c
suryatejaj97 <45379824+suryatejaj97@users.noreply.github.com>
2020-05-13 17:43:20 +0530

    class and instance variable

---
+++ python11.py
@@ -0,0 +1,18 @@
+# Class and Instance variable
+
+class employee:
+    company = 'IT Soft Sol'             #Class Variable
+
+    def __init__(self,name,empid):
+        self.name = name
+        self.empid = empid
+    def display(self):
+        print('Employee Name: ',self.name)
+        print('Employee ID: ',self.empid)
+        print('Company: ',employee.company)
+
+employee1 = employee('Jim Anderson','ITSS898')
+employee1.display()
+
+employee2 = employee('Arthor John','ITSS68')
+employee2.display()
